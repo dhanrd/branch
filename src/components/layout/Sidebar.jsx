e9a3283a26fdbf0f46800e4b730da826e678245c
sofie-m <sofie.curtis@ucalgary.ca>
2025-04-05 15:13:32 -0600

    sidebar initial re-design & addition of "see all" button to groups section.

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -12,7 +12,15 @@ export default function Sidebar() {
   const dummyGroups = [
     { id: 1, name: 'MACHINE LEARNING' },
     { id: 2, name: 'CSUS' },
-    { id: 3, name: 'WEB DEVELOPMENT' }
+    { id: 3, name: 'WEB DEVELOPMENT' },
+    { id: 4, name: 'GAMING' },
+    { id: 5, name: 'STUDY TIPS' },
+    { id: 6, name: 'BASKETBALL' },
+    { id: 7, name: 'BASEBALL' },
+    { id: 8, name: 'COMPUTER GRAPHICS' },
+    { id: 9, name: 'COMPETITIVE PROGRAMMING' }
+
+
   ];
 
   if (!user) return null;
@@ -24,25 +32,56 @@ export default function Sidebar() {
     { name: 'JOB BOARD', path: '/dashboard/jobs' },
   ];
 
+ 
   return (
-    <div className="w-64 h-screen bg-[#1a1a1a] flex flex-col border-r border-[#3a3a3a]">
-      <div className="p-4 mb-2">
-        <h1 className="text-xl font-bold text-white">Branch</h1>
+    <div className="w-80 h-screen-0 bg-[#1a1a1a] flex flex-col border-r border-[#3a3a3a]">
+      <div className="p-4 mb-4">
+        <h1 className="text-2xl font-bold text-white">Branch</h1>
       </div>
       
+      <nav className="px-4 mb-4">
+        {navItems.map((item) => (
+          <Link 
+            key={item.path}
+            href={item.path}
+            className={`block p-4 mb-4 w-58 h-12 left-[32px] relative rounded-md shadow-[0px_1px_15px_0px_rgba(17,17,17,1.00)] overflow-hidden 
+              uppercase text-center justify-center font-bold leading-tight tracking-wide text-white ${
+              pathname === item.path 
+                ? 'bg-[#303030] shadow-[inset_0px_2px_4px_0px_rgba(0,0,0,0.25)]' 
+                : 'bg-[#6C6C6C] hover:bg-[#555555]'
+            }`}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </nav>
+
+      <div className="border-2 border-[#3a3a3a] mb-4 mx-4"></div>
+
       {user.role === 'student' && (
         <>
           <div className="px-4 mb-2">
-            <h2 className="text-[#888888] mb-2">GROUPS</h2>
-            <div className="h-28 overflow-y-auto pr-2 mb-2">
+
+            <h2 className="flex justify-between text-[#868686] font-extrabold uppercase leading-tight tracking-wide mb-4">GROUPS
+              <div className="mt-0.5 flex justify-between">
+                <div className="h-6 text-right text-[#E4E4E4] text-sm font-bold uppercase leading-tight tracking-wide">See All </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="6 3.5 12 20" fill="#E4E4E4" className="size-6">
+                  <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                </svg>
+              </div>
+            </h2>
+
+
+            <div className="max-h-3/4 overflow-y-auto pr-2 mb-2">
               {dummyGroups.map((group) => (
                 <Link 
                   href={`/dashboard/groups/${group.id}`}
                   key={group.id}
-                  className={`block p-4 mb-3 rounded text-center text-white uppercase font-medium ${
+                  className={`block p-4 mb-4 w-58 h-18 left-[32px] relative rounded-md shadow-[0px_1px_15px_0px_rgba(17,17,17,1.00)] overflow-hidden 
+                    uppercase text-center justify-center font-bold leading-tight content-center tracking-wide text-white  ${
                     pathname === `/dashboard/groups/${group.id}` 
-                      ? 'bg-[#444444]' 
-                      : 'bg-[#666666]'
+                      ? 'bg-[#303030] shadow-[inset_0px_2px_4px_0px_rgba(0,0,0,0.25)]' 
+                      : 'bg-[#6C6C6C] hover:bg-[#555555]'
                   }`}
                 >
                   {group.name}
@@ -51,32 +90,18 @@ export default function Sidebar() {
             </div>
             <Link
               href="/dashboard/groups"
-              className="flex items-center justify-center p-2 text-[#4caf9e] border border-[#444444] rounded mb-4 hover:bg-[#333333]"
+              className="mt-3 flex items-center justify-center p-2 text-[#4caf9e] border border-[#444444] rounded mb-4 hover:bg-[#333333]"
             >
               + FIND MORE
             </Link>
           </div>
-          <div className="border-b border-[#3a3a3a] mb-4 mx-4"></div>
+          
         </>
       )}
       
-      <nav className="flex-1 px-4">
-        {navItems.map((item) => (
-          <Link 
-            key={item.path}
-            href={item.path}
-            className={`block p-3 mb-4 rounded uppercase text-white ${
-              pathname === item.path 
-                ? 'bg-[#333333]' 
-                : 'bg-[#444444] hover:bg-[#555555]'
-            }`}
-          >
-            {item.name}
-          </Link>
-        ))}
-      </nav>
+
       
-      <div className="mt-auto border-t border-[#3a3a3a] p-4">
+      <div className="absolute bottom-0 w-80 mt-auto border-t border-[#3a3a3a] p-4">
         <Link href="/dashboard/profile">
           <div className="flex items-center mb-2 cursor-pointer hover:bg-[#222222] p-2 rounded-md transition-colors">
             <div className="w-10 h-10 rounded-full bg-[#555555] mr-3"></div>
@@ -89,12 +114,13 @@ export default function Sidebar() {
         
         <button 
           onClick={logout}
-          className="flex items-center mt-2 text-red-400"
+          className="flex items-center mt-2 text-red-400 hover:text-red-200 cursor-pointer"
         >
-          <span className="rounded-full w-6 h-6 flex items-center justify-center bg-black mr-2">N</span>
-          out
+          logout
         </button>
       </div>
     </div>
+
+
   );
 }
